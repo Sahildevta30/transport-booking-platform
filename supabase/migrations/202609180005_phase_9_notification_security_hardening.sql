80abd45303c25e3f52c63c32b29f0fd3f6fdebc6
Sahil Kumar Gupta <suryasahilsskg@gmail.com>
2026-09-18 00:34:13 +0530

    security: protect notification trigger functions

---
+++ supabase/migrations/202609180005_phase_9_notification_security_hardening.sql
@@ -0,0 +1,2 @@
+revoke execute on function public.notify_booking_status_change() from public,anon,authenticated;
+revoke execute on function public.notify_payment_status_change() from public,anon,authenticated;
